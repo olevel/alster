e0441b7ff5ce1ad039a7435de5d0fdd1fc14eb7b
olevel <hacker.olevel@gmail.com>
2017-09-13 10:34:42 +0000

    index page ui laid out

---
+++ js/my_js.js
@@ -0,0 +1,18 @@
+/**
+ * Created by olevel on 9/12/17.
+ */
+
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        slideWidth: 1080,
+        minSlides: 1,
+        maxSlides: 1,
+        slideMargin: 0,
+        controls: false,
+        auto: true,
+        autoControls: false,
+        pause: 5000,
+        captions: true,
+        speed: 1000
+    });
+});
